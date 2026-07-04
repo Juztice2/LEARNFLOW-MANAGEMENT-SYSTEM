e8342be8cb2337ee67fa2a8d86487330039d8853
juztice2 <juzticewilliam@gmail.com>
2026-07-04 12:56:13 +0100

    make the signin & signup feuture

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,36 +1,76 @@
 import { Link, NavLink } from "react-router-dom";
+import {auth} from "../firebase.js"
+import { signInWithEmailAndPassword } from "firebase/auth";
 import smallteam from "../assets/assets_learn_flow/small-team.png"
 import { BookOpen } from "lucide-react";
-export default function Signin(){
+import { useState } from "react";
+
+ function Signin (){
+  
+   const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState("");
+
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  setLoading(true);
+  setMsg('');
+  try{
+    await signInWithEmailAndPassword(auth, email, password);
+    setMsg('Login successful! You are now signed login')
+  }catch (err){
+setMsg('err.message');
+  }finally {
+    setLoading(false)
+  }
+ }
   return(
    <div className="overflow-hidden bg-[#F5F5F5]">
        <div className="flex gap-2 justify-self-center items-center mt-8 mb-8">
     <BookOpen size={30} />
    <Link to="/" className="font-poppins text-3xl   hover:text-slate-400 cursor-pointer active:text-slate-50">LearnFlow</Link>
    </div>
-   <div className=" md:flex px-10">
-    <form className="bg-white/95 rounded-lg p-7 shadow-md shadow-black justify-self-center w-[70%] mb-8 lg:w-[80%]">
-   <h2 className="text-3xl font-poppins font-light">Welcome !</h2>
+   <div className="m-2 md:flex">
+    <div className=" mb-8 lg:w-[70%]">
+    <form onSubmit={handleSubmit} className="bg-white/95 rounded-lg p-3  shadow-md shadow-black justify-self-center w-[70%]">
+   <h2 className=" font-poppins text-purple-700 justify-self-center">< BookOpen size={50}/></h2>
    <h3 className="text-4xl font-dm  mt-5  ">Sign in to</h3>
      <p className="text-lg font-poppins">LearnFlow University</p>
 
    <div className="mt-8 grid gap-2 font-poppins font-semibold">
-   <label for="name">User name</label>
-   <input placeholder="Enter your user name" id="name"  className="border border-neutral-800 p-4 rounded-lg"></input>
+   <label for="email">Email</label>
+   <input placeholder="Enter your email" id="email" type="email" value={email} onChange={(e)=> setEmail(e.target.value)}
+    required 
+    className="border border-neutral-800 p-4 rounded-lg"></input>
    </div>
+
+   {/*<div className="mt-8 grid gap-2 font-poppins font-semibold">
+   <label for="name">User name</label>
+   <input placeholder="Enter your user name" id="name" type="text" value={user} onChange={(e)=> setUser(e.target.value)} required   className="border border-neutral-800 p-4 rounded-lg"></input>
+   </div>*/}
 
    <div className="mt-8 grid gap-2 font-poppins font-semibold">
    <label for="password">Password</label>
-   <input placeholder="Enter your user password" id="password" className="border border-neutral-800 p-4 rounded-lg"></input>
+   <input placeholder="Enter your user password" id="password" type="password"  value={password} onChange={(e)=> setPassword(e.target.value)} required  className="border border-neutral-800 p-4 rounded-lg"></input>
    </div>
-   <div>
+
+   <div className="mt-8 grid gap-2 font-poppins font-semibold">
+   <label for="comfirm-password">Comfirm Password</label>
+   <input placeholder="Comfirm your password" id="comfirm-password" className="border border-neutral-800 p-4 rounded-lg"></input>
    </div>
-   <button className="bg-black text-white p-4 w-full mt-6 rounded-lg font-bold font-poppins hover:bg-slate-400 active:bg-white">Login</button>
-    <p className=" font-poppins justify-self-center mt-3 mb-4 text-lg text-stone-500 ">Don't have an Account ? <Link to="/signup" className="text-black font-bold hover:cursor-pointer">Register</Link></p>
+   <button disabled={loading} type="submit" className="bg-black text-white p-4 w-full mt-6 rounded-lg font-bold font-poppins hover:bg-slate-400 active:bg-white">
+    {loading ? "..." : 'Log In'} 
+    </button>
    </form>
-    <img src={smallteam} className="hidden md:block"/>
+   {msg && <p className="font-poppins p-2 bg-green-400 text-white mt-3 justify-self-center rounded-md">{msg}</p>}
+    <p className=" font-poppins justify-self-center mt-3 mb-4 text-lg text-stone-500 ">Don't have an Account ? <Link to="/signup" className="text-black font-bold hover:cursor-pointer">Create a free account</Link></p>
+    </div>
+   <img src={smallteam} className="hidden md:block w-[40%]"/>
    </div>
    </div>
   )
 }
+export default Signin
 
