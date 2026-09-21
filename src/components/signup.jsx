5eac10b24419db35e9ea3218c0ab92867f2e0a9b
juztice2 <juzticewilliam@gmail.com>
2026-09-21 13:18:29 +0100

    adjusting the login image

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -89,6 +89,7 @@ import { useAuth } from "../Context/authcontext.jsx";
     <p className=" font-poppins justify-self-center mt-3 mb-4  text-stone-500 text-sm ">Already have an Account ? <Link to="/signin" className="text-black font-bold hover:cursor-pointer">Login</Link></p>
   </form>
    <div>
+       <img src={smallteam} className="hidden lg:block justify-self-center mt-32 w-[70%]"/>
    </div>
  </div>
   )
